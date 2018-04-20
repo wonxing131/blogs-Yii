a6066f99aac64f5acea0cc0aedca7ae069540b5c
dtc <dingtongchuan@outlook.com>
2018-04-21 01:49:10 +0800

    将后台模版更换为adminlte,重新初始化项目

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,10 +13,6 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'vendor/font-awesome/css/font-awesome.min.css',
-        'vendor/nprogress/nprogress.css',
-        'vendor/animate.css/animate.min.css',
-        'css/custom.min.css'
     ];
     public $js = [
     ];
@@ -24,26 +20,4 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
-
-    /**
-     * 在最后添加CSS文件
-     * @param object $view view文件
-     * @param resource $cssFile css文件
-     * @return mixed
-     */
-    public static function addCss($view,$cssFile)
-    {
-        return $view->registerCssFile($cssFile,[AppAsset::className(),'depends'=>'backend\assets\AppAsset']);
-    }
-
-    /**
-     * 在最后添加JS文件
-     * @param object $view view 文件
-     * @param resource $jsFile js文件
-     * @return mixed
-     */
-    public static function addScript($view,$jsFile)
-    {
-        return $view->registerJsFile($jsFile,[AppAsset::className(),'depends'=>'backend\assets\AppAsset']);
-    }
 }
